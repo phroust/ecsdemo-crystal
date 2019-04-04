6908269cf01939197ef44e0c6291b0741a4cb78c
EC2 Default User <ec2-user@ip-172-31-22-28.us-west-2.compute.internal>
2019-04-04 11:50:03 +0000

    test

--- src/server.cr
+++ src/server.cr
@@ -25,7 +25,7 @@ server = HTTP::Server.new(
     ]) do |context|
       if context.request.path == "/crystal" || context.request.path == "/crystal/"
         context.response.content_type = "text/plain"
-        context.response.print "Crystal backend: Hello! from #{az_message} commit #{code_hash}"
+        context.response.print "TEST Crystal backend: Hello! from #{az_message} commit #{code_hash}"
       elsif context.request.path == "/health"
         context.response.content_type = "text/plain"
         context.response.print "Healthy!"
